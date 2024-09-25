15a58f7df8a708a24017e8fcef3d2aeebea8707c
puf520 <noemite92@gmail.com>
2024-09-25 11:02:44 +0200

    clase customer terminada

--- MyLibrary/customer.cs
+++ MyLibrary/customer.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace MyLibrary
 {
     internal class customer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
